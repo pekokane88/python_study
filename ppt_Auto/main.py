8be1250e8a7bda7774fcc6f9c21d0aa0969cc8cc
pekokane88 <108170898+pekokane88@users.noreply.github.com>
2022-08-22 17:33:35 +0900

    Update main.py

--- ppt_Auto/main.py
+++ ppt_Auto/main.py
@@ -1,30 +1,54 @@
 from PySide6.QtWidgets import QApplication, QMainWindow, QFileDialog, QMessageBox
+from PySide6.QtGui import QIcon
 from mainWindow import Ui_MainWindow
 from pptx import Presentation
 from datetime import datetime
 import PIL
 from PIL import ImageGrab, Image
 import pyautogui
+import icon
 
 #아직 구현하지 못한 것.
 # 1. clean code
+# 2. 다중 슬라이드 생성 및 편집.
+# 3. 이미지 파일 사용 후 제거?
+# 4. 외관? -> self.setWindowIcon() 해서 로고 이미지 추가.
 
 class mainWindow(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super(mainWindow, self).__init__()
         self.setupUi(self)
+        self.init_ui()
+        self.show_ver.triggered.connect(self.show_inform)
+        self.close_window.triggered.connect(self.close_all)
 
+    # show logo image
+    def init_ui(self):
+        self.setWindowTitle('AutoPPT')
+        self.setWindowIcon(QIcon(':/logo.png'))
+        # setGeomety (X, Y, Width, Height)
+        self.setGeometry(400, 100, 500, 500)
+        self.show()
+
+    def close_all(self):
+        QMessageBox.about(self, "End", "BYE!")
+        QApplication.closeAllWindows()
+
+    def show_inform(self):
+        QMessageBox.about(self, "Version", "Demo 0.0.3r")
+
+    # make ppt - This is a sample code.
+    # Input your ppt code here.
     def make_ppt(self):
         try:
+            # your code Start --------------------------------------
             title = self.titleEdit.text()
             inform = self.informEdit.toPlainText()
             now = datetime.now()
-            current_time = now.strftime("%Y-%m-%d %H:%M:%S")
-            current_text = current_time + '\n' + inform
+            current_time = now.strftime("%Y-%m-%d %H:%M:%S\n")
+            current_text = current_time + inform
             img_path = self.imgLabel.text()
 
-            # 본인의 base가 될 ppt를 지정하여서 slide에 있는 shape를 지정해주고 그 shape 의 type에 맞게 데이터를 넣어주면 된다.
-            # 여기서 shape 란 slide 안에 있는 content를 의미한다. ex) 이미지 박스, 제목 박스, 텍스트 박스 
             prs = Presentation('base.pptx')
             main_slide = prs.slides[0]
 
@@ -33,24 +57,25 @@ class mainWindow(QMainWindow, Ui_MainWindow):
             for i, shape in enumerate(shape_list):
                 shape_index[shape.name] = i
 
-            # 제목
+            # ppt Title
             title_select = shape_list[shape_index['title_box']].text_frame
             title_select.text = title
 
-            # 사진
+            # ppt Image
             img_select = shape_list[shape_index["img_box"]]
             img_select.insert_picture(img_path)
 
-            # inform
+            # ppt Main-text
             text_select = shape_list[shape_index['text_box']].text_frame
             text_select.text = current_text
             
-            # 파일 저장 양식
-            output = current_time + '.pptx'
+            # Setting output
+            output = now.strftime('%Y-%m-%d %H_%M') + '.pptx'
             prs.save(output)
             QMessageBox.about(self, "About", "Work Done!")
             QApplication.closeAllWindows()
-        # 예외 처리
+            # your code end----------------------------------------------------
+        # Setting Exception
         except FileNotFoundError:
             QMessageBox.warning(self, "Error!", "There is no image File.\n Please select image file or capture screen.")
         except PIL.UnidentifiedImageError:
@@ -79,6 +104,7 @@ class mainWindow(QMainWindow, Ui_MainWindow):
         im = im.resize((644, 400))
         im = im.convert('RGB')
         im.save(img_name, 'PNG')
+
 
 app = QApplication()
 win = mainWindow()
